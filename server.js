d3b7c4a9179db5bcc943c8d139e7d522fd1c00c6
brightstudent <zaid.el.boustani@hva.nl>
2022-04-21 12:44:34 +0200

    local server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const express = require('express');
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! This is Zaid. How are you doing?')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
