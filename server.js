09664787bf688c4fbf96ebfb982f79b24596d8b1
brightstudent <z.elboustani@hotmail.com>
2022-04-29 17:26:45 +0200

    server opgezet

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+app.use(express.static("assets"));
+
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send('Hello World! This is Zaid. How are you doing?')
+  res.render('index')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World! This is Zaid. How are you doing?')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
