de58c94ed625899c6e64c0879aab0544c8599bba
brightstudent <z.elboustani@hotmail.com>
2022-04-29 17:28:32 +0200

    server opgezet

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.get('/', (req, res) => {
 //   res.send('Hello World! This is Zaid. How are you doing?')
 // })
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
