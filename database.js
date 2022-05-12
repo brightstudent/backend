abb6577b823c85a75221f1b0572781ac932659a2
brightstudent <z.elboustani@hotmail.com>
2022-05-13 01:28:10 +0200

    more pictures added

--- database.js
+++ database.js
@@ -3,21 +3,26 @@ const restaurants = [
     picture: ["/static/pic2.jpg", "/static/pic3.jpg"],
     name: "Poke magic",
     location: "2.5 km",
-    slug: 'poke-magic',
+    slug: "poke-magic",
     menu: [
       {
-        name: 'Fany bowl',
+        name: "Fany bowl",
         price: 10.95,
-        image: '/static/'
-      }
-    ]
+        image: "/static/",
+      },
+    ],
   },
   {
-    picture: ["https://images.unsplash.com/photo-1508615263227-c5d58c1e5821?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-    , "https://images.unsplash.com/photo-1586058584825-c1e87ed735b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80"],
+    picture: [
+      "https://images.unsplash.com/photo-1508615263227-c5d58c1e5821?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      "https://images.unsplash.com/photo-1586058584825-c1e87ed735b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80",
+      "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      "https://images.unsplash.com/photo-1623316405245-8418193f2fc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    ],
     name: "BBQ Meat",
     location: "3.1 km",
-    slug: 'bbq-meat'
+    slug: "bbq-meat",
   },
 ];
 
