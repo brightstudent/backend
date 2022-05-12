20450e542ea22578b38e3a042e4b66d81f9c701a
brightstudent <z.elboustani@hotmail.com>
2022-05-13 01:02:37 +0200

    foto's ophalen van een link

--- database.js
+++ database.js
@@ -13,7 +13,8 @@ const restaurants = [
     ]
   },
   {
-    picture: ["/static/pic4.jpg", "/static/pic5.jpg"],
+    picture: ["https://images.unsplash.com/photo-1508615263227-c5d58c1e5821?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    , "https://images.unsplash.com/photo-1586058584825-c1e87ed735b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80"],
     name: "BBQ Meat",
     location: "3.1 km",
     slug: 'bbq-meat'
