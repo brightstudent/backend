7435dd3b8dd0d50a5a9b917ef476b045396166e5
brightstudent <z.elboustani@hotmail.com>
2022-05-13 01:28:38 +0200

    scroll knoppen toegevoegd aan de carousel

--- assets/drag.js
+++ assets/drag.js
@@ -1,33 +1,41 @@
-
-const slider = document.querySelector('#images');
+const slider = document.querySelector("#images");
+const previousBtn = document.querySelector("#option .previous");
+const nextBtn = document.querySelector("#option .next");
 
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('active')
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
 
-slider.addEventListener('mouseleave', (e) => {
-    isDown = false;
-    slider.classList.remove('active')
+slider.addEventListener("mouseleave", (e) => {
+  isDown = false;
+  slider.classList.remove("active");
 });
 
-slider.addEventListener('mouseup', (e) => {
-    isDown = false;
-    slider.classList.remove('active')
+slider.addEventListener("mouseup", (e) => {
+  isDown = false;
+  slider.classList.remove("active");
 });
 
-slider.addEventListener('mousemove', (e) => {
-    if(!isDown) return; // stop function from running.
-    e.preventDefault
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 2;
-    slider.scrollLeft = scrollLeft - walk;
-    console.log(walk)
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return; // stop function from running.
+  e.preventDefault;
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
 });
 
+nextBtn.addEventListener("click", () => {
+  slider.scrollLeft += 200;
+  console.log('next')
+});
+previousBtn.addEventListener("click", () => {
+  slider.scrollLeft -= 300;
+});
