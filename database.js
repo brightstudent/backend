6e92bd21fda6a228644ad75a4723476a5f7d02da
brightstudent <z.elboustani@hotmail.com>
2022-05-13 00:33:36 +0200

    min-width aan de images gegeven

--- database.js
+++ database.js
@@ -3,7 +3,14 @@ const restaurants = [
     picture: ["/static/pic2.jpg", "/static/pic3.jpg"],
     name: "Poke magic",
     location: "2.5km",
-    slug: 'poke-magic'
+    slug: 'poke-magic',
+    menu: [
+      {
+        name: 'Fany bowl',
+        price: 10.95,
+        image: '/static/'
+      }
+    ]
   },
   {
     picture: ["/static/pic4.jpg", "/static/pic5.jpg"],
