a6dd432eb7ede0d3d2d671bab7d2f693125e36e2
brightstudent <z.elboustani@hotmail.com>
2022-05-13 10:22:18 +0200

    added 404 page

--- server.js
+++ server.js
@@ -1,41 +1,44 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const restaurants = require('./database');
-const favs = {}
-app.use(express.json())
+const restaurants = require("./database");
+const favs = {};
+app.use(express.json());
 app.use(express.static("assets"));
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  console.log(restaurants)
-  res.render('pages/index', { restaurants })
-})
+app.get("/", (req, res) => {
+  // console.log(restaurants)
+  res.render("pages/index", { restaurants });
+});
 
-app.get('/favorites', (req, res) => {
-  res.render('pages/favorites', {restaurants: Object.values(favs)})
-})
+app.get("/favorites", (req, res) => {
+  res.render("pages/favorites", { restaurants: Object.values(favs) });
+});
 
-app.get('/restaurants/:slug', (req, res) => {
-  const result = restaurants.find(r => r.slug == req.params.slug)
-  res.render('pages/detail-page', {restaurant: result})
-})
+app.get("/restaurants/:slug", (req, res) => {
+  const result = restaurants.find((r) => r.slug == req.params.slug);
+  res.render("pages/detail-page", { restaurant: result });
+});
 
-app.get('/api/restaurants', (req, res) => {
+app.get("/api/restaurants", (req, res) => {
   res.json(restaurants);
-})
+});
 
-app.post('/api/favorites', (req, res) => {
+app.post("/api/favorites", (req, res) => {
   // TODO: save favorites
-  const restaurant  = req.body
-  favs[restaurant.name] = restaurant
-  res.status(204).send()
-})
-
+  const restaurant = req.body;
+  favs[restaurant.name] = restaurant;
+  res.status(204).send();
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-}) 
-// {name: res, name: res}
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.use(function (req, res, next) {
+  // res.status(404).send(`that doesn't exist`);
+  res.render("pages/404");
+});
