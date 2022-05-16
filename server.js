ba84f01f4997a86dae04cb89be8668539746a8f8
brightstudent <z.elboustani@hotmail.com>
2022-05-16 10:05:41 +0200

    require dotenv

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require('dotenv').config()
+
 const restaurants = require("./database");
 const favs = {};
 app.use(express.json());
