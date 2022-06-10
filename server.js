2031f66f8273208cb700e553ec2d7ead2a10ce6f
brightstudent <z.elboustani@hotmail.com>
2022-06-10 13:51:28 +0200

    Used ESLint styling

--- server.js
+++ server.js
@@ -18,68 +18,68 @@ app.use(express.static("assets"));
 app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
-  const restaurants = await db.collection("restaurants").find({}, {}).toArray();
-  res.render("pages/index", { restaurants, homepage: true });
+	const restaurants = await db.collection("restaurants").find({}, {}).toArray();
+	res.render("pages/index", { restaurants, homepage: true });
 });
 
 app.get("/login", async (req, res) => {
-  const api = unsplash.createApi({
-    accessKey: process.env.UNSPLASH_KEY,
-    fetch,
-  });
-  const splashResponse = await api.search.getPhotos({query: 'food', page: 1, perPage: 10});
-  const pics = splashResponse.response.results
-  const randomIndex = Math.floor(Math.random() * 43) % pics.length
-  res.render("pages/login", 
-    {backgroundImage: pics[randomIndex].urls.regular}
-  );
+	const api = unsplash.createApi({
+		accessKey: process.env.UNSPLASH_KEY,
+		fetch,
+	});
+	const splashResponse = await api.search.getPhotos({query: "food", page: 1, perPage: 10});
+	const pics = splashResponse.response.results;
+	const randomIndex = Math.floor(Math.random() * 43) % pics.length;
+	res.render("pages/login", 
+		{backgroundImage: pics[randomIndex].urls.regular}
+	);
 });
 
 
 app.get("/favorites", async (req, res) => {
-  let favs = await db
-    .collection("favorites")
-    .find({ user: req.query.email }, { projection: { favorite: 1 } })
-    .toArray();
-  favs = favs.map((e) => ObjectId(e.favorite));
-  const restaurants = await db
-    .collection("restaurants")
-    .find({ _id: { $in: favs } })
-    .toArray();
-  res.render("pages/favorites", {
-    restaurants,
-    homepage: false,
-  });
+	let favs = await db
+		.collection("favorites")
+		.find({ user: req.query.email }, { projection: { favorite: 1 } })
+		.toArray();
+	favs = favs.map((e) => ObjectId(e.favorite));
+	const restaurants = await db
+		.collection("restaurants")
+		.find({ _id: { $in: favs } })
+		.toArray();
+	res.render("pages/favorites", {
+		restaurants,
+		homepage: false,
+	});
 });
 
 app.get("/restaurants/:slug", async (req, res) => {
-  // const result = restaurants.find((r) => r.slug == req.params.slug);
-  const result = await db
-    .collection("restaurants")
-    .findOne({ slug: req.params.slug }, {});
-  res.render("pages/detail-page", { restaurant: result });
+	// const result = restaurants.find((r) => r.slug == req.params.slug);
+	const result = await db
+		.collection("restaurants")
+		.findOne({ slug: req.params.slug }, {});
+	res.render("pages/detail-page", { restaurant: result });
 });
 
 app.get("/api/restaurants", async (req, res) => {
-  // this is how i get my data from a certain collection from db
-  const places = await db.collection("restaurants").find({}, {}).toArray();
-  res.json(places);
+	// this is how i get my data from a certain collection from db
+	const places = await db.collection("restaurants").find({}, {}).toArray();
+	res.json(places);
 });
 
 app.post("/api/favorites", async (req, res) => {
-  const saveRequest = req.body;
-  try {
-    await db.collection("favorites").insert(saveRequest);
-  } catch (e) {
-    console.log(e);
-  }
-  res.status(204).send();
+	const saveRequest = req.body;
+	try {
+		await db.collection("favorites").insert(saveRequest);
+	} catch (e) {
+		console.log(e);
+	}
+	res.status(204).send();
 });
 
 async function connectDB() {
-  try {
-    // mongoDB / my database ophalen
-    const uri =
+	try {
+		// mongoDB / my database ophalen
+		const uri =
       "mongodb+srv://" +
       process.env.DB_USERNAME +
       ":" +
@@ -89,27 +89,27 @@ async function connectDB() {
       "/" +
       process.env.DB_NAME +
       "?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverApi: ServerApiVersion.v1,
-    });
-    await client.connect();
-    db = client.db(process.env.DB_NAME);
-  } catch (error) {
-    throw error;
-  }
+		const client = new MongoClient(uri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			serverApi: ServerApiVersion.v1,
+		});
+		await client.connect();
+		db = client.db(process.env.DB_NAME);
+	} catch (error) {
+		throw error;
+	}
 }
 
 // Define as last route
 app.use(function (req, res, next) {
-  // res.status(404).send(`that doesn't exist`);
-  res.render("pages/404");
+	// res.status(404).send(`that doesn't exist`);
+	res.render("pages/404");
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
-  connectDB().then(console.log(connectDB));
+	console.log(`Example app listening on port ${process.env.PORT}`);
+	connectDB().then(console.log(connectDB));
 });
 
 // 1. dynamic back button based on page location / slug
